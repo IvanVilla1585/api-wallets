facc2e07fac033078c99a543911d0c1bcaece6d6
Ivan Dario Villa Ramirez <trezeguet55@gmail.com>
2022-08-10 15:17:58 -0500

    feat: add new config

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,35 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      stopAtFirstError: true,
+      forbidUnknownValues: false,
+      skipUndefinedProperties: false,
+      skipMissingProperties: false,
+      validationError: {
+        target: true,
+        value: true,
+      },
+    }),
+  );
+
+  const config = new DocumentBuilder()
+    .setTitle('Virtual Wallets')
+    .setDescription('A user can have virtual wallets')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/documentation', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
