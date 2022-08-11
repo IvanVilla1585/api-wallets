5b6c64332ff502443dd037a5e5055cd0061d00bb
Ivan Dario Villa Ramirez <trezeguet55@gmail.com>
2022-08-10 21:28:47 -0500

    feat: create user property

--- src/Auth/dtos/UserAuthResponseDto.ts
+++ src/Auth/dtos/UserAuthResponseDto.ts
@@ -2,7 +2,10 @@ import { ApiProperty } from '@nestjs/swagger';
 
 import { UserBaseDto } from '../../Dtos/UserBaseDto';
 
-export class UserAuthResponseDto extends UserBaseDto {
+export class UserAuthResponseDto {
   @ApiProperty({ required: true })
   token: string;
+
+  @ApiProperty({ required: true })
+  user: UserBaseDto;
 }
