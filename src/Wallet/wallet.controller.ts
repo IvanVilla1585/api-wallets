6947c6abeb839f511aaf0de9326c0b1e73d76787
Ivan Dario Villa Ramirez <trezeguet55@gmail.com>
2022-08-10 21:41:49 -0500

    feat: use new type

--- src/Wallet/wallet.controller.ts
+++ src/Wallet/wallet.controller.ts
@@ -11,7 +11,7 @@ import { ApiTags, ApiResponse, ApiBearerAuth, ApiParam } from '@nestjs/swagger';
 
 import { WalletService } from './wallet.service';
 import { JwtAuthGuard } from '../guards/jwt.guard';
-import { WalletDto, WalletResponseDto } from './dtos';
+import { WalletRequestDto, WalletResponseDto } from './dtos';
 
 @ApiBearerAuth()
 @ApiTags('Wallets')
@@ -22,16 +22,21 @@ export class WalletController {
   @ApiResponse({ status: 201, type: WalletResponseDto })
   @UseGuards(JwtAuthGuard)
   @Post()
-  create(@Body() data: WalletDto, @Request() req): Promise<WalletResponseDto> {
-    data.userId = req.user.id;
-    return this.walletService.create(data);
+  create(
+    @Body() data: WalletRequestDto,
+    @Request() req,
+  ): Promise<WalletResponseDto> {
+    return this.walletService.create({ ...data, userId: req.user.id });
   }
 
   @ApiResponse({ status: 200, type: WalletResponseDto })
   @ApiParam({ name: 'id', type: 'string' })
   @UseGuards(JwtAuthGuard)
   @Patch(':id')
-  update(@Body() data: WalletDto, @Param() params): Promise<WalletResponseDto> {
+  update(
+    @Body() data: WalletRequestDto,
+    @Param() params,
+  ): Promise<WalletResponseDto> {
     return this.walletService.update(params.id, data.balance_usd);
   }
 }
